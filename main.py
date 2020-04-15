54b121fbac6e2f54909d5a930c793a089c9c5f01
alenatorgasheva <in-fa@mail.ru>
2020-04-15 18:39:28 +0700

    Add function data_petrol_stations.

--- main.py
+++ main.py
@@ -5,9 +5,29 @@
 #              Zemtseva A.       (%),
 #              Torgasheva A.     (%).
 
+import codecs
+
+
+def data_petrol_stations():
+    """
+    Getting data about petrol stations.
+    :return: dictionary with data about petrol stations
+    """
+    petrol_stations = {}
+    with codecs.open('azs.txt', 'r', encoding='utf-8') as file_in:
+        for string in file_in.readlines():
+            string = string.split()
+            station_number = string[0]
+            queue_length = string[1]
+            petrol_stations[station_number] = {}
+            petrol_stations[station_number]['queue'] = queue_length
+            petrol_stations[station_number]['kinds'] = string[2:]
+
+    return petrol_stations
+
 
 def main():
-    pass
+    petrol_stations = data_petrol_stations()
 
 
 main()
